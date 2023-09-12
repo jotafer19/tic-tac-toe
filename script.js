47f2d2a8e40d7b30f2c94cdfde7b51941c07f1c4
jotafer19 <115662522+jotafer19@users.noreply.github.com>
2023-09-12 16:55:47 +0200

    Add event listeners and general play function

--- script.js
+++ script.js
@@ -18,67 +18,86 @@ const gameBoard = (() => {
 
 const gameController = (() => {
     let _turnCount = 1;
+    let _gameOver = false;
 
     const showTurn = () => _turnCount;
 
     const addTurn = () => _turnCount += 1;
+    
+    const getMark = () => (_turnCount & 2 !== 0) ? "X" : "O"
 
-    const resetGameController = () => _turnCount = 0;
+    const resetGameController = () => {
+        _turnCount = 1;
+        _gameOver = false;
+    }
 
     const checkWinner = (gameBoard, player) => {
-        for (let i = 0; i < gameBoard.length - 2; i++) {
+        for (let i = 0; i < gameBoard.length - 2 ; i+=3) {
             if (gameBoard[i] === gameBoard[i + 1] && gameBoard[i] === gameBoard[i + 2] && gameBoard[i] !== "") {
                 console.log(`${player} wins!`)
-                return true;
+                _gameOver = true;
             }
         }
         for (let i = 0; i < 3; i++) {
             if (gameBoard[i] === gameBoard[i + 3] && gameBoard[i] === gameBoard[i + 6] && gameBoard[i] !== "") {
                 console.log(`${player} wins!`)
-                return true;
+                _gameOver = true;
             }
         }
         if (gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8] && gameBoard[4] !== "") {
             console.log(`${player} wins!`)
-            return true;
+            _gameOver = true;
         }
         if (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6] && gameBoard[4] !== "") {
             console.log(`${player} wins!`)
-            return true;
+            _gameOver = true;
         }
     }
 
-    return {showTurn, addTurn, resetGameController, checkWinner}
+    const check_gameOver = () => _gameOver;
+
+    return {showTurn, addTurn, getMark, resetGameController, checkWinner, check_gameOver}
 })();
 
-const playerFactory = (playerPosition) => {
+const playerFactory = ((mark) => {
 
-    const playerMark = () => (playerPosition === "PlayerOne") ? "X" :"O";
-    
-    return {playerPosition, playerMark};
-}
+    const getPlayer = (mark) => (mark === "X") ? "Player 1" : "Player 2";
+
+    return {mark, getPlayer};
+})()
 
 const playGame = (() => {
-    let haveAWinner = false;
+    const positionButtons = document.querySelectorAll(".choice-spot");
+    const resetButton = document.querySelector("#reset");
 
-    const playerOne = playerFactory("PlayerOne");
-    const playerTwo = playerFactory("PlayerTwo");
-
-    while (!haveAWinner) {
+    const letsPlayTheGame = (spot) => {
         console.log(gameController.showTurn());
-        if (gameController.showTurn() % 2 !== 0) {
-            gameBoard.addMark(prompt("Choose a position: "), playerOne.playerMark())
-            haveAWinner = gameController.checkWinner(gameBoard.showGameBoard(), playerOne.playerPosition)
-        } else {
-            gameBoard.addMark(prompt("Choose a position: "), playerTwo.playerMark())
-            haveAWinner = gameController.checkWinner(gameBoard.showGameBoard(), playerTwo.playerPosition)
-        }
+        gameBoard.addMark(spot.dataset.index, gameController.getMark());
+        spot.textContent = gameController.getMark();
+        gameController.checkWinner(gameBoard.showGameBoard(), playerFactory.getPlayer(gameController.getMark()));
+
         console.log(gameBoard.showGameBoard())
         gameController.addTurn();
 
-        if (gameController.showTurn() > 10 && !haveAWinner) {
+        if (gameController.showTurn() > 9 && !haveAWinner) {
             console.log("It's a tie!")
             haveAWinner = true;
         }
     }
+
+    const resetGame = () => {
+        gameBoard.resetGameBoard();
+        gameController.resetGameController();
+        positionButtons.forEach(button => button.textContent = "");
+        gameController
+    }
+
+    positionButtons.forEach(button => {
+        button.addEventListener("click", (event) => {
+            if (event.target.textContent !== "" || gameController.check_gameOver()) return;
+            letsPlayTheGame(event.target);
+        })
+    })
+
+    resetButton.addEventListener("click", resetGame)
 })()
