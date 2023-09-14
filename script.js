d14843e3737c1c6fa8b5a84b8faa0b107716fd38
jotafer19 <115662522+jotafer19@users.noreply.github.com>
2023-09-14 11:04:00 +0200

    Add score panel

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const gameController = (() => {
 
     const getPlayer = (playerOne, playerTwo) => (_turnCount % 2 !== 0) ? playerOne : playerTwo;
 
-    const resetGameController = () => {
+    const newRound = () => {
         _turnCount = 1;
         turnDisplay.style.display = "block";
         turnDisplay.textContent = "Player 1's turn";
@@ -42,36 +42,74 @@ const gameController = (() => {
         _gameOver = false;
     }
 
+    const resetGameController = () => {
+        newRound();
+        playerOneScore = 0;
+        displayPlayerOneScore.textContent = 0;
+        playerTwoScore = 0;
+        displayPlayerTwoScore.textContent = 0;
+        tieScore = 0;
+        displayTieScore.textContent = 0;
+    }
+
     const checkWinner = (gameBoard, player) => {
         for (let i = 0; i < gameBoard.length - 2 ; i+=3) {
             if (gameBoard[i] === gameBoard[i + 1] && gameBoard[i] === gameBoard[i + 2] && gameBoard[i] !== "") {
                 winnerAnnouncer.showModal();
-                winnerPlayer.textContent = `${player} wins!`.toUpperCase();
+                winnerPlayer.textContent = `${player.name} wins!`.toUpperCase();
+                addPoint(player.mark);
                 _gameOver = true;
             }
         }
         for (let i = 0; i < 3; i++) {
             if (gameBoard[i] === gameBoard[i + 3] && gameBoard[i] === gameBoard[i + 6] && gameBoard[i] !== "") {
                 winnerAnnouncer.showModal();
-                winnerPlayer.textContent = `${player} wins!`.toUpperCase();
+                winnerPlayer.textContent = `${player.name} wins!`.toUpperCase();
+                addPoint(player.mark);
                 _gameOver = true;
             }
         }
         if (gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8] && gameBoard[4] !== "") {
             winnerAnnouncer.showModal();
-            winnerPlayer.textContent = `${player} wins!`.toUpperCase();
+            winnerPlayer.textContent = `${player.name} wins!`.toUpperCase();
+            addPoint(player.mark);
             _gameOver = true;
         }
         if (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6] && gameBoard[4] !== "") {
             winnerAnnouncer.showModal();
-            winnerPlayer.textContent = `${player} wins!`.toUpperCase();
+            winnerPlayer.textContent = `${player.name} wins!`.toUpperCase();
+            addPoint(player.mark);
             _gameOver = true;
+        }
+        if (!_gameOver && _turnCount === 9) {
+            winnerAnnouncer.showModal();
+            winnerPlayer.textContent = "It's a tie!"
+            tieScore += 1;
+            displayTieScore.textContent = tieScore;
+        }
+    }
+
+    let playerOneScore = 0;
+    const displayPlayerOneScore = document.querySelector("#player-one-score > .points");
+    let playerTwoScore = 0;
+    const displayPlayerTwoScore = document.querySelector("#player-two-score > .points");
+    let tieScore = 0;
+    const displayTieScore = document.querySelector("#tie > .points");
+
+    const addPoint = (playerMark) => {
+        if (playerMark === "X") {
+            playerOneScore += 1;
+            displayPlayerOneScore.textContent = playerOneScore;
+        } 
+        if (playerMark === "O") {
+            playerTwoScore += 1;
+            displayPlayerTwoScore.textContent = playerTwoScore;
         }
     }
 
     const checkGameOver = () => _gameOver;
 
-    return {showTurn, addTurn, displayTurn, getPlayer, resetGameController, checkWinner, checkGameOver}
+    return {showTurn, addTurn, displayTurn, getPlayer, newRound, resetGameController, checkWinner, checkGameOver}
 })();
 
 const getPlayerData = (() => {
@@ -100,7 +138,7 @@ const playGame = (() => {
 
     const winnerAnnouncer = document.querySelector("#winner-announcer");
     const closeDialog = document.querySelector("#close-dialog");
-    const newRound = document.querySelector("#new-round");
+    const newRoundButton = document.querySelector("#new-round");
 
     const playerOne = playerFactory(getPlayerData.getPlayerOneName(), "X");
     const playerTwo = playerFactory(getPlayerData.getPlayerTwoName(), "O");
@@ -109,16 +147,17 @@ const playGame = (() => {
         
         gameBoard.addMark(spot.dataset.index, gameController.getPlayer(playerOne, playerTwo).mark);
         spot.textContent = gameController.getPlayer(playerOne, playerTwo).mark;
-        gameController.checkWinner(gameBoard.showGameBoard(), gameController.getPlayer(playerOne, playerTwo).name);
+        gameController.checkWinner(gameBoard.showGameBoard(), gameController.getPlayer(playerOne, playerTwo));
 
         console.log(gameBoard.showGameBoard())
         gameController.addTurn();
         gameController.displayTurn(gameController.getPlayer(playerOne, playerTwo).name);
+    }
 
-        if (gameController.showTurn() > 9 && !gameController.checkGameOver()) {
-            console.log("It's a tie!")
-            haveAWinner = true;
-        }
+    const newRound = () => {
+        gameBoard.resetGameBoard();
+        gameController.newRound();
+        positionButtons.forEach(button => button.textContent = "");
     }
 
     const resetGame = () => {
@@ -140,8 +179,8 @@ const playGame = (() => {
         winnerAnnouncer.close();
     })
 
-    newRound.addEventListener("click", () => {
-        resetGame();
+    newRoundButton.addEventListener("click", () => {
+        newRound();
         winnerAnnouncer.close();
     });
 })()
