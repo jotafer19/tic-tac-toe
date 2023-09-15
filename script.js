ab83c40abd4502852ed932bc8b31f32b96827d89
jotafer19 <115662522+jotafer19@users.noreply.github.com>
2023-09-16 00:59:28 +0200

    Add smartphone support

--- script.js
+++ script.js
@@ -16,171 +16,154 @@ const gameBoard = (() => {
     return {showGameBoard, addMark, resetGameBoard}
 })();
 
+const playerFactory = (name, mark) => {
+    let _points = 0;
+
+    const showScore = () => _points;
+
+    const addPoint = () => _points += 1;
+    
+    const resetScore = () => _points = 0;
+
+    const displayPoints = (playerScoreDisplay) => playerScoreDisplay.textContent = _points;
+
+    return {name, mark, showScore, addPoint, resetScore, displayPoints};
+}
+
 const gameController = (() => {
-    let _turnCount = 1;
+    const turnInformation = document.querySelector("#turn-information");
+    const playerOneScore = document.querySelector("#player-one-score > .points");
+    const playerTwoScore = document.querySelector("#player-two-score > .points");
+    const draw = document.querySelector("#draw > .points");
+    const winnerDialog = document.querySelector("#winner-announcer");
+    const winMessage = document.querySelector("#winner");
+
+    let _turn = 1;
     let _gameOver = false;
+    let _drawScore = 0;
 
-    const matchInformation = document.querySelector("#match-information");
-    const turnDisplay = document.querySelector("#turn-information");
-    const winnerAnnouncer = document.querySelector("#winner-announcer");
-    const winnerPlayer = document.querySelector("#winner");
+    const addTurn = () => _turn += 1;
 
+    const getPlayer = (playerOne, playerTwo) => (_turn % 2 !== 0) ? playerOne : playerTwo;
 
-    const showTurn = () => _turnCount;
+    const displayTurn = (playerOne, playerTwo) => turnInformation.textContent = `${getPlayer(playerOne, playerTwo).name} turn`
 
-    const addTurn = () => _turnCount += 1;
+    const checkGameOver = () => _gameOver;
 
-    const displayTurn = (player) => turnDisplay.textContent = `${player}'s turn`
+    const updateScore = (player) => {
+        player.addPoint();
+        (player.mark === "X") ? playerOneScore.textContent = player.showScore() : playerTwoScore.textContent = player.showScore();
+    }
 
-    const getPlayer = (playerOne, playerTwo) => (_turnCount % 2 !== 0) ? playerOne : playerTwo;
-
-    const newRound = () => {
-        _turnCount = 1;
-        turnDisplay.style.display = "block";
-        turnDisplay.textContent = "Player 1's turn";
-        matchInformation.textContent = "";
+    const newRoundGameController = () => {
+        turnInformation.textContent = "Player 1 turn";
+        _turn = 1;
         _gameOver = false;
     }
 
     const resetGameController = () => {
-        newRound();
-        playerOneScore = 0;
-        displayPlayerOneScore.textContent = 0;
-        playerTwoScore = 0;
-        displayPlayerTwoScore.textContent = 0;
-        tieScore = 0;
-        displayTieScore.textContent = 0;
+        newRoundGameController();
+        playerOneScore.textContent = 0;
+        playerTwoScore.textContent = 0;
+        _drawScore = 0;
+        draw.textContent = 0;
     }
 
     const checkWinner = (gameBoard, player) => {
         for (let i = 0; i < gameBoard.length - 2 ; i+=3) {
             if (gameBoard[i] === gameBoard[i + 1] && gameBoard[i] === gameBoard[i + 2] && gameBoard[i] !== "") {
-                winnerAnnouncer.showModal();
-                winnerPlayer.textContent = `${player.name} wins!`.toUpperCase();
-                addPoint(player.mark);
+                winnerDialog.showModal();
+                winMessage.textContent = (`${player.name} wins!`).toUpperCase();
+                updateScore(player);
                 _gameOver = true;
             }
         }
         for (let i = 0; i < 3; i++) {
             if (gameBoard[i] === gameBoard[i + 3] && gameBoard[i] === gameBoard[i + 6] && gameBoard[i] !== "") {
-                winnerAnnouncer.showModal();
-                winnerPlayer.textContent = `${player.name} wins!`.toUpperCase();
-                addPoint(player.mark);
+                winnerDialog.showModal();
+                winMessage.textContent = (`${player.name} wins!`).toUpperCase();
+                updateScore(player);
                 _gameOver = true;
             }
         }
         if (gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8] && gameBoard[4] !== "") {
-            winnerAnnouncer.showModal();
-            winnerPlayer.textContent = `${player.name} wins!`.toUpperCase();
-            addPoint(player.mark);
+            winnerDialog.showModal();
+            winMessage.textContent = (`${player.name} wins!`).toUpperCase();
+            updateScore(player);
+            _gameOver = true;
+        } else if (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6] && gameBoard[4] !== "") {
+            winnerDialog.showModal();
+            winMessage.textContent = (`${player.name} wins!`).toUpperCase();
+            updateScore(player);
+            _gameOver = true;
+        } else if (!checkGameOver() && _turn === 9) {
+            winnerDialog.showModal();
+            winMessage.textContent = ("It's a draw!").toUpperCase();
+            _drawScore += 1;
+            draw.textContent = _drawScore;
             _gameOver = true;
         }
-        if (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6] && gameBoard[4] !== "") {
-            winnerAnnouncer.showModal();
-            winnerPlayer.textContent = `${player.name} wins!`.toUpperCase();
-            addPoint(player.mark);
-            _gameOver = true;
-        }
-        if (!_gameOver && _turnCount === 9) {
-            winnerAnnouncer.showModal();
-            winnerPlayer.textContent = "It's a tie!"
-            tieScore += 1;
-            displayTieScore.textContent = tieScore;
-        }
     }
-
-    let playerOneScore = 0;
-    const displayPlayerOneScore = document.querySelector("#player-one-score > .points");
-    let playerTwoScore = 0;
-    const displayPlayerTwoScore = document.querySelector("#player-two-score > .points");
-    let tieScore = 0;
-    const displayTieScore = document.querySelector("#tie > .points");
-
-    const addPoint = (playerMark) => {
-        if (playerMark === "X") {
-            playerOneScore += 1;
-            displayPlayerOneScore.textContent = playerOneScore;
-        } 
-        if (playerMark === "O") {
-            playerTwoScore += 1;
-            displayPlayerTwoScore.textContent = playerTwoScore;
-        }
-    }
-
-    const checkGameOver = () => _gameOver;
-
-    return {showTurn, addTurn, displayTurn, getPlayer, newRound, resetGameController, checkWinner, checkGameOver}
-})();
-
-const getPlayerData = (() => {
-    const playerOneName = document.querySelector("#player-one-name");
-    const playerTwoName = document.querySelector("#player-two-name");
-
-    const getPlayerOneName = () => (playerOneName.value === "") ? "Player 1" : playerOneName.value;
-    const getPlayerTwoName = () => (playerTwoName.value === "") ? "Player 2" : playerTwoName.value;
-
-    const resetPlayerData = () => {
-        playerOneName.value = "";
-        playerTwoName.value = "";
-    }
-
-    return {getPlayerOneName, getPlayerTwoName, resetPlayerData}
+    return {addTurn, getPlayer, displayTurn, checkGameOver, newRoundGameController, resetGameController, checkWinner}
 })()
 
-const playerFactory = (name, mark) => {
-
-    return {name, mark};
-}
-
 const playGame = (() => {
-    const positionButtons = document.querySelectorAll(".choice-spot");
+    const playSpots = document.querySelectorAll(".choice-spot");
     const resetButton = document.querySelector("#reset");
-
-    const winnerAnnouncer = document.querySelector("#winner-announcer");
+    const winnerDialog = document.querySelector("#winner-announcer");
     const closeDialog = document.querySelector("#close-dialog");
     const newRoundButton = document.querySelector("#new-round");
 
-    const playerOne = playerFactory(getPlayerData.getPlayerOneName(), "X");
-    const playerTwo = playerFactory(getPlayerData.getPlayerTwoName(), "O");
+    const playerOne = playerFactory("Player 1", "X");
+    const playerTwo = playerFactory("Player 2", "O");
 
-    const letsPlayTheGame = (spot) => {
-        
+    const displayMark = (spot, mark) => {
+        spot.textContent = mark;
+        if (mark === "X") {
+            spot.style.color = "#fcd34d";
+        } else {
+            spot.style.color = "#93c5fd";
+        }
+    }
+
+    const playTurn = (spot) => {
         gameBoard.addMark(spot.dataset.index, gameController.getPlayer(playerOne, playerTwo).mark);
-        spot.textContent = gameController.getPlayer(playerOne, playerTwo).mark;
-        gameController.checkWinner(gameBoard.showGameBoard(), gameController.getPlayer(playerOne, playerTwo));
-
-        console.log(gameBoard.showGameBoard())
-        gameController.addTurn();
-        gameController.displayTurn(gameController.getPlayer(playerOne, playerTwo).name);
+        displayMark(spot, gameController.getPlayer(playerOne, playerTwo).mark);
+        gameController.checkWinner(gameBoard.showGameBoard(), gameController.getPlayer(playerOne, playerTwo))
+        if (!gameController.checkGameOver()) {
+            gameController.addTurn();
+            gameController.displayTurn(playerOne, playerTwo);
+        }
     }
 
     const newRound = () => {
         gameBoard.resetGameBoard();
-        gameController.newRound();
-        positionButtons.forEach(button => button.textContent = "");
+        gameController.newRoundGameController();
+        playSpots.forEach(spot => spot.textContent = "");
     }
 
     const resetGame = () => {
-        gameBoard.resetGameBoard();
+        newRound();
+        playerOne.resetScore();
+        playerTwo.resetScore();
         gameController.resetGameController();
-        positionButtons.forEach(button => button.textContent = "");
     }
 
-    positionButtons.forEach(button => {
-        button.addEventListener("click", (event) => {
+    playSpots.forEach(spot => {
+        spot.addEventListener("click", (event) => {
             if (event.target.textContent !== "" || gameController.checkGameOver()) return;
-            letsPlayTheGame(event.target);
+            playTurn(event.target);
         })
     })
 
     resetButton.addEventListener("click", resetGame);
 
     closeDialog.addEventListener("click", () => {
-        winnerAnnouncer.close();
+        winnerDialog.close();
     })
 
     newRoundButton.addEventListener("click", () => {
         newRound();
-        winnerAnnouncer.close();
-    });
+        winnerDialog.close();
+    })
 })()
